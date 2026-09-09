dc6710decd691ea0fca179e3566fdd49da7798c1
aji <2442733838@qq.com>
2026-09-09 19:31:59 +0800

    为前端集成vue-router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHashHistory} from "vue-router";
 import {createPinia} from "pinia";
 
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+});
 const pinia = createPinia();
 
 const app = createApp(App);
+app.use(router);
 app.use(pinia);
 app.mount('#app');
